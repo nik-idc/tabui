c7938eb335b55878fcb0e5e3cf21e75f9f0262f4
tikina <nikvmain@gmail.com>
2025-05-20 15:13:56 +0300

    Update Score class

--- src/models/score.ts
+++ src/models/score.ts
@@ -6,36 +6,50 @@ import { Track } from "./track";
  */
 export class Score {
   /**
+   * Score's id
+   */
+  readonly id: number;
+  /**
+   * Score's name
+   */
+  public name: string;
+  /**
+   * Score's song artist
+   */
+  public artist: string;
+  /**
+   * Score's song name
+   */
+  public songName: string;
+  /**
+   * Public status
+   */
+  readonly isPublic: boolean;
+  /**
+   * Score's trakcs. NOTE: For now only support guitar tabs.
+   * In the far, far future support for classical musical notation &
+   * thus non-guitar tracks will be added (hopefully)
+   */
+  readonly tracks: Tab[];
+
+  /**
    * Class for a score for a song. Contains all the tracks for the song
    */
   constructor(
-    /**
-     * Score's id
-     */
-    readonly id: number = -1,
-    /**
-     * Score's name
-     */
-    public name: string = "Unknown",
-    /**
-     * Score's song artist
-     */
-    public artist: string = "Unknown artist",
-    /**
-     * Score's song name
-     */
-    public songName: string = "Unknown song",
-    /**
-     * Public status
-     */
-    readonly isPublic: boolean = false,
-    /**
-     * Score's trakcs. NOTE: For now only support guitar tabs.
-     * In the far, far future support for classical musical notation &
-     * thus non-guitar tracks will be added (hopefully)
-     */
-    readonly tracks: Tab[]
-  ) {}
+    id: number = -1,
+    name: string = "Unknown",
+    artist: string = "Unknown artist",
+    songName: string = "Unknown song",
+    isPublic: boolean = false,
+    tracks: Tab[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.artist = artist;
+    this.songName = songName;
+    this.isPublic = isPublic;
+    this.tracks = tracks;
+  }
 
   /**
    * Create a Score object from a JSON object
